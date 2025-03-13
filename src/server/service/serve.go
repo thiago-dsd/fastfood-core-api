afedf91f712f0ae553de2e2f5a9c684607f87dde
Thiago Dias <thiago.dsd@outlook.com>
2025-03-12 22:01:28 -0300

    Add origin in CORS config.

--- src/server/service/serve.go
+++ src/server/service/serve.go
@@ -13,9 +13,12 @@ import (
 func Serve() {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Content-Type,Authorization",
+		AllowCredentials: true,
 	}))
-
+	
 	makeDocs(app)
 	user_router.Route(app)
 
